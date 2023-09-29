3cced8f47fb8d905c942ef3cf8df12a7def4dce9
Яна Курзюкова <kurzukova.yana@yandex.ru>
2023-09-29 12:11:29 +0300

    start project

---
+++ send_revenue/revenue/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from models import RevenueStatistic
+
+
+admin.site.register(RevenueStatistic)
+
---
+++ send_revenue/send/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from models import SpendStatistic
+
+
+admin.site.register(SpendStatistic)
+
